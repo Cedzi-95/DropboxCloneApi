821cf5ab54d158fb2fae650ad388d9f49f5e373d
Cedzi-95 <cedricmasinzo@yahoo.com>
2025-05-15 10:01:15 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+
+namespace DropboxCloneApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+       
+        var app = builder.Build();
+
+       
+
+        app.Run();
+    }
+}
